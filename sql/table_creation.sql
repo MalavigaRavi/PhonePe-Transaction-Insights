52274d280020d62a8429cd20d62782c0d34cda1e
MalavigaRavi <149233118+MalavigaRavi@users.noreply.github.com>
2026-02-14 12:40:13 +0530

    Create aggregated_transaction table in SQL

--- sql/table_creation.sql
+++ sql/table_creation.sql
@@ -1,1 +1,10 @@
-
+TABLE1: aggregated_transacion
+CREATE TABLE aggregated_transacion (
+    state TEXT,
+    year INT,
+    quarter INT,
+    transaction_type TEXT,
+    transaction_count BIGINT,
+    transaction_amount DOUBLE PRECISION
+);
+TABLE2: 
